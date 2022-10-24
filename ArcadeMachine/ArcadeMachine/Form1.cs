960fa753f15592decec9e54c13dd10ab60996743
twgust <gustavzfloren@gmail.com>
2022-10-24 11:30:57 +0200

    Implemented Unity - WinForms integration

--- ArcadeMachine/ArcadeMachine/Form1.cs
+++ ArcadeMachine/ArcadeMachine/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace ArcadeMachine
 {
     public partial class Form1 : Form
@@ -10,6 +12,23 @@ namespace ArcadeMachine
         private void PictureBox2_Click(object sender, EventArgs e)
         {
             
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // define path for a unity game built for WINDOWS. 
+            startGame("C:\\Users\\sweet\\Desktop\\game\\My project.exe");
+        }
+        private void startGame(String path)
+        {
+            var hWnd = this.Handle;
+            String str = hWnd.ToString();
+            ProcessStartInfo info = new ProcessStartInfo();
+
+            info.FileName = (path);
+            info.Arguments = "--parentHWND " + hWnd;
+            Process.Start(info);
         }
     }
 }
