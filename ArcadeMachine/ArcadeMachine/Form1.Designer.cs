3b6526478f288bf7e0e4a36d5eceeec6a2a08b6a
twgust <gustavzfloren@gmail.com>
2022-10-27 12:42:46 +0200

    added event handler

--- ArcadeMachine/ArcadeMachine/Form1.Designer.cs
+++ ArcadeMachine/ArcadeMachine/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // panel2
             // 
