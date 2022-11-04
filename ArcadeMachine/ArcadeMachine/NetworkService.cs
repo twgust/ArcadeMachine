53a92b3a7420eadea3d0e21fd09fda69a7518f73
twgust <gustavzfloren@gmail.com>
2022-11-04 17:33:17 +0100

    Refractor NetworkService

--- ArcadeMachine/ArcadeMachine/NetworkService.cs
+++ ArcadeMachine/ArcadeMachine/NetworkService.cs
@@ -1,29 +1,23 @@
-﻿using System.Net.Sockets;
-using System.Net;
-using System.Runtime.CompilerServices;
+﻿using ArcadeMachineLibrary;
 using System.Diagnostics;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
-using ArcadeMachineLibrary;
+using System.Runtime.Serialization.Formatters.Binary;
 namespace ArcadeMachine
 {
     internal class NetworkService
-
     {
         private string hostIPAddress { get; }
         private int port { get; }
-
         private Thread t;
-
-     
-
 
         public NetworkService(int port, String ip)
         {
             this.port = port;
             this.hostIPAddress = ip; 
         }
-
 
         [MethodImpl(MethodImplOptions.Synchronized)]
         public void startServer()
@@ -48,36 +42,29 @@ namespace ArcadeMachine
                 t.Abort();
             }
         }
+
         public void init()
         {
             try
             {
-            
                 IPAddress ip = IPAddress.Parse(hostIPAddress);
                 TcpListener listener = new TcpListener(ip, port);
                 listener.Start();
                 Debug.WriteLine("Server online...\nAwaiting client connection...");
                 TcpClient client = listener.AcceptTcpClient();
                 Debug.WriteLine("Client connected!");
-               
-
-                
-                
  
                 IFormatter formatter = new BinaryFormatter();
-                    NetworkStream stream = client.GetStream();
+                NetworkStream stream = client.GetStream();
                 SharedData obj = (SharedData) formatter.Deserialize(stream);
-                
-               
+              
                 Debug.WriteLine("KEY: " + obj.getKey());
                 Debug.WriteLine("VALUE: " + obj.getValue());
-                    // notify with callback 
-                    
+                // notify with callback 
+                  
                 stream.Close();
                 client.Close();
-                
             }
-
             catch (SerializationException e)
             {
                 Debug.WriteLine(e.Message);         
